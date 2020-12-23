e910dc22f4c075d2df9c0fc2cf339b1578bc3c0b
Jennifer Morales <jennifer.morales1201@gmail.com>
2020-12-22 20:06:04 -0500

    Installed Bulma and create first component

---
+++ src/components/ToDo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+  <div class="notification is-primary">
+    This container is <strong>centered</strong> on desktop and larger viewports.
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'ToDo'
+  }
+</script>
+
+<style></style>
