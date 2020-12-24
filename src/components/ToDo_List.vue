5f08056aab82a845ee9db8dd1b23cb261e05cde6
Jennifer Morales <jennifer.morales1201@gmail.com>
2020-12-23 20:22:51 -0500

    Added a button and a todo item box

--- src/components/ToDo_List.vue
+++ src/components/ToDo_List.vue
@@ -1,5 +1,23 @@
 <template>
-  <input type="text" />
+<div>
+  <div class="field is-grouped">
+  <p class="control is-expanded">
+    <input class="input" type="text" placeholder="Add ToDo Item">
+  </p>
+  <p class="control">
+    <a class="button is-info">
+      Add
+    </a>
+  </p>
+  <div class="content">
+</div>
+</div>
+ <div class="notification has-background-primary">
+  <button class="delete"></button>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit
+</div>
+
+</div>
 </template>
 
 <script>
