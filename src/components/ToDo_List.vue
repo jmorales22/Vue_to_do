c6bd7398badaeba0583b6fe3cd62fc3d3c134efa
Jennifer Morales <jennifer.morales1201@gmail.com>
2020-12-23 20:56:06 -0500

    Added the ability to delete a todo from the todo list

--- src/components/ToDo_List.vue
+++ src/components/ToDo_List.vue
@@ -1,29 +1,55 @@
 <template>
-<div>
-  <div class="field is-grouped">
-  <p class="control is-expanded">
-    <input class="input" type="text" placeholder="Add ToDo Item">
-  </p>
-  <p class="control">
-    <a class="button is-info">
-      Add
-    </a>
-  </p>
-  <div class="content">
-</div>
-</div>
- <div class="notification has-background-primary">
-  <button class="delete"></button>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit
-</div>
-
-</div>
+  <div>
+    <div class="field is-grouped">
+      <p class="control is-expanded">
+        <input class="input" type="text" placeholder="Add ToDo Item" />
+      </p>
+      <p class="control">
+        <a class="button is-info">
+          Add
+        </a>
+      </p>
+      <div class="content"></div>
+    </div>
+    <div
+      class="notification has-background-primary"
+      v-for="todo in todos"
+      :key="todo.text"
+    >
+      <button class="delete" v-on:click="deleteToDoListItem(todo.text)"></button>
+      {{ todo.text }}
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'ToDo_List'
-  }
+export default {
+  name: 'ToDo_List',
+  data: function() {
+    return {
+      todos: [
+        {
+          text: 'finish to do list',
+        },
+        {
+          text: 'clean the house',
+        },
+        {
+          text: 'wash the car',
+        },
+      ],
+    };
+  },
+  methods: {
+    deleteToDoListItem: function(toDoText) {
+        for(let i = 0; i < this.todos.length; i++){
+            if(this.todos[i].text === toDoText){
+                this.todos.splice(i, 1);
+            }
+        }
+    },
+  },
+};
 </script>
 
 <style></style>
